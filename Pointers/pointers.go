d8f6e752883c46ed3ea2c75f3fb880192cc3e4e2
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-21 22:51:19 +0530

    add pointers.go with examples of pointer declaration and dereferencing in Go

---
+++ Pointers/pointers.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int = 10
+	fmt.Println(num)
+
+	// Declare a pointer to an integer
+	var ptr *int
+	ptr = &num
+
+	fmt.Println(ptr)
+
+	// Pointer Dereference
+	fmt.Println(*ptr)
+
+
+	num = 100
+	*ptr = 200
+
+	fmt.Println(&ptr)
+}
