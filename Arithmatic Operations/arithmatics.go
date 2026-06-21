f97c45eb7dc45cb01920b645cb19bc1615d3d436
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-21 21:43:37 +0530

    add arithmatics.go with examples of bitwise operations

---
+++ Arithmatic Operations/arithmatics.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// a := 10
+	// b := 3
+
+	// sum := a + b
+	// difference := a - b
+	// product := a * b
+	// quotient := a / b
+	// remainder := a % b
+
+	// fmt.Println("Sum:", sum)
+	// fmt.Println("Difference:", difference)
+	// fmt.Println("Product:", product)
+	// fmt.Println("Quotient:", quotient)
+	// fmt.Println("Remainder:", remainder)
+
+	// Compound assignment operators
+	// c := 5
+
+	// c += 2
+	// fmt.Println(c)
+
+	// c -= 2
+	// fmt.Println(c)
+
+	// c /=2
+	// fmt.Println(c)
+
+	// c *=2
+	// fmt.Println(c)
+
+	// c := 5
+	// c++
+	// c--
+	// fmt.Println(c)
+
+
+
+	// Bitwise operators
+
+	fmt.Println(5 & 3)
+	fmt.Println(5 | 3)
+
+}
